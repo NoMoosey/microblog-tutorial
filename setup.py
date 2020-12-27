f40c68d72724f15a3ec02d372ac132d68be75a20
Joe Loesch <joey.loesch@gmail.com>
2020-12-27 13:36:42 -0500

    Created migrations

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='flask-tutorial',
     author='Joe Loesch',
     author_email='joey.loesch@gmail.com',
-    packages='microblog',
     description='Following a flask tutorial...',
     install_requires=[
-        "Flask == 1.1.2",
-        "Flask-WTF == 0.14.3",
-        "python-dotenv == 0.15.0",
-    ],
+        "Flask >= 1.1.2",
+        "Flask-WTF >= 0.14.3",
+        "python-dotenv >= 0.15.0",
+        "Flask-SQLAlchemy >= 2.4.4",
+    ]
     )
