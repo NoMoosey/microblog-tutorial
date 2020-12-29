579870e8bda39cde72f152c2f818037394fffaa4
Joe <joey.loesch@gmail.com>
2020-12-29 18:04:47 -0500

    Chapter 15; (#10)
    Refactor of app structure

--- .flaskenv
+++ .flaskenv
@@ -1,16 +1,6 @@
 # Production Variables
 FLASK_APP=microblog.py
 
-MAIL_SERVER=smtp.googlemail.com
-MAIL_PORT=587
-MAIL_USE_TLS=1
-MAIL_USERNAME=joe.wr.loesch@gmail.com
-
 # Developer Variables
 FLASK_DEBUG=1
 LOG_LEVEL=DEBUG
-#MAIL_SERVER=localhost
-#MAIL_PORT=8025
-#MAIL_USE_TLS=
-#MAIL_USERNAME=
-#MAIL_PASSWORD=
--- app/translate.py
+++ app/translate.py
@@ -1,14 +1,16 @@
 import json
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
+
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in app.config or \
-            not app.config['MS_TRANSLATOR_KEY']:
+    current_app.logger.debug('Translating "{}" from {} to {}'.format(text, source_language, dest_language))
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
     auth = {
-        'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY'],
         'Ocp-Apim-Subscription-Region': 'eastus'}
     r = requests.post(
         'https://api.cognitive.microsofttranslator.com'
@@ -16,5 +18,6 @@ def translate(text, source_language, dest_language):
             source_language, dest_language), headers=auth, json=[
                 {'Text': text}])
     if r.status_code != 200:
+        current_app.logger.error('Translation failed with status code {}'.format(r.status_code))
         return _('Error: the translation service failed.')
     return r.json()[0]['translations'][0]['text']
