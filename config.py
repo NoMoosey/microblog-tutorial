0f61845463fa47a8c2a2d0cd9addf67a403aba60
Joe Loesch <joey.loesch@gmail.com>
2020-12-30 14:20:23 -0500

    Removed sqlite logic

--- config.py
+++ config.py
@@ -10,8 +10,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # SQL DB Configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # E-mail notification settings
